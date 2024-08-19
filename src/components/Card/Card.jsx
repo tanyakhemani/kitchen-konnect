1b5a438f4acf5fa2259f65527e8fc857318e8b93
tanyakhemani <tanyakhemani.12@gmail.com>
2024-08-19 19:27:54 -0400

    fix card image style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Card.scss"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Card = ({ card }) => {
+
+    const [recipeImage, setRecipeImage] = useState();
+
+    const loadRecipeImage = async() => {
+        const response = await axios.get(`http://localhost:8080/api/recipes/${card.id}/images`)
+        const imgData = response.data;
+        let image = "/public/vite.svg";
+
+        if(imgData){
+            image = `data:image/${imgData.image_type};base64,${imgData.image}`;
+        }
+
+        setRecipeImage(image);
+    }
+
+    useEffect(()=>{
+        loadRecipeImage();
+    }, [card])
 
     return(
         <>
             <Link className="card" to={`/view/${card.id}`}>
-                <img className="card__img" src={card?.image || "/public/vite.svg"} alt="" />
+                <img className="card__img" src={recipeImage} alt="" />
                 <div className="card__title">{card?.title || 'Some title'}</div>
             </Link>
         </>
